e5809b4d9b68c2877a7fec82d21990a034e1e6ba
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2025-05-30 11:37:02 +0200

    Initial commit

---
+++ updates/create_academy_user_fields.php
@@ -0,0 +1,29 @@
+<?php namespace academyusers\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateAcademyUserFields extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->string('organization')->nullable();
+            $table->string('city_state')->nullable();
+            $table->text('reason_for_joining')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn([
+                'organization',
+                'city_state',
+                'reason_for_joining'
+            ]);
+        });
+    }
+} 
